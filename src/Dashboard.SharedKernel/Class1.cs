a43f29eef99eda46c1d6ef3a3f5d9ee7f084dd3a
Lenny <lpervin@gmail.com>
2023-07-29 12:05:46 -0400

    Initial Commit

---
+++ src/Dashboard.SharedKernel/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace Dashboard.SharedKernel;
+
+public class Class1
+{
+}
---
+++ tests/Dashboard.Web.API.Test/UnitTest1.cs
@@ -0,0 +1,9 @@
+namespace Dashboard.Web.API.Test;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
