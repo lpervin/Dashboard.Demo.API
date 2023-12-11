473daa4d1f59a60e99b85e668a3616e09f4f1764
Lenny <lpervin@gmail.com>
2023-12-10 20:15:49 -0500

    Unversioned

---
+++ src/AWSPlayGround/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+// CreateAWSCloudWatchGroup();
+//
+// void CreateAWSCloudWatchGroup()
+// {
+//     //throw new NotImplementedException();
+// }
